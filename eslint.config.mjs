6a6b452517323a9ceb2c33e98ce4ef7d2b13cbc3
sunshumin <sunshumin@kuaishou.com>
2025-03-21 17:19:54 +0800

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,6 +39,14 @@ export default [
       // 禁用 import/prefer-default-export 规则
       'import/prefer-default-export': 0,
       'import/no-extraneous-dependencies': 0,
+      'no-use-before-define': 0,
+      'no-shadow': 0,
+      'no-restricted-syntax': 0,
+      'no-return-assign': 0,
+      'no-param-reassign': 0,
+      'no-sequences': 0,
+      'no-loop-func': 0,
+      'no-nested-ternary': 0,
     },
   },
 ];
